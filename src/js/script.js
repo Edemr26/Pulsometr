361ff9ac0b6b7f2ed833c824c0247837a006cb58
Edemr26 <ug_stroyinvest77@mail.ru>
2023-09-19 19:23:18 +0300

    mailer

--- src/js/script.js
+++ src/js/script.js
@@ -74,4 +74,21 @@ $(document).ready(function () {
     valideForms('#consultation-form');
     valideForms('#consultation form');
     valideForms('#buy form');
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
